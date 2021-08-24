5b882c314c38996ec7ab8ce6ec10a655079988b2
Ruslan <ruslan5665@gmail.com>
2021-08-25 00:07:49 +0300

    bundle splitter load screen

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import { register } from 'react-native-bundle-splitter';
 import Home from '../screens/Home'
+import LoadScreen from '../screens/load/'
 
 const Stack = createStackNavigator();
 
@@ -9,6 +11,7 @@ function MainStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="LoadScreen" component={LoadScreen} />
     </Stack.Navigator>
   );
 }
---
+++ src/screens/load/LoadScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const LoadScreen = () => {
+  return (
+    <View>
+      <Text>LoadScreen</Text>
+    </View>
+  )
+}
+
+export default LoadScreen
+
+const styles = StyleSheet.create({})
